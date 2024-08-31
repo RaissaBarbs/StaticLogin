5eef1c376886c415d744c45341f7e9b228ae9905
RaissaBarbs <raissabarbosacs@gmail.com>
2024-08-31 10:51:56 -0300

    a

---
+++ StaticLogin/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StaticLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private const string StaticUsername = "Raíssa";
+        private const string StaticPassword = "1234";
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == StaticUsername && password == StaticPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Usuário ou Senha inválidos!";
+            return View();
+        }
+    }
+}
---
+++ StaticLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StaticLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
